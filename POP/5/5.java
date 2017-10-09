bf950b25699aa9ec0931b7d058c80fa4ac8d40fa
Srinag Rao S <srinskit@users.noreply.github.com>
2017-10-09 18:31:13 +0530

    Update 5.java
    fixed missmatch

--- POP/5/5.java
+++ POP/5/5.java
@@ -3,21 +3,36 @@ class MutualFund{
 		int aadhar;
 	}
 	Customer customer;
+	String name;
+	final void printWarning(){
     final void printWarning(){
 		System.out.println("Mutual funds are subject to market risks.\nRead all scheme related docs carefully.");
     }
+    MutualFund(String name){
+		this.name = name;    	
+    }
+    void printName(){
+    	System.out.println(name);
+    }
 }
 class myMutualFund extends MutualFund{
-	/*class Custom extends Customer{
-		int email;	
+	// class Custom extends Customer{
+	// 	int email;	
+	// }
+	// final void printWarning(){
+	// 	System.out.println("Mutual funds are not subject to market risks.");
+	// }
+	myMutualFund(String name){
+		super(name);    	
+    }
+	void printName(){
+		System.out.println("NITK/"+name);
 	}
-	final void printWarning(){
-		System.out.println("Mutual funds are not subject to market risks.");
-	}*/
 }
 class p5{
 	public static void main(String args[]){
-		myMutualFund fund = new myMutualFund();
+		myMutualFund fund = new myMutualFund("NorthernTrust");
 		fund.printWarning();
+		fund.printName();
 	}
 }
