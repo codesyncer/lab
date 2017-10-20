6482c40e7cb382be22e28e262793749dd98cf933
srinskit <srinagrao2007@gmail.com>
2017-10-20 12:30:10 +0530

    query larger than string bugfix + PEP8 compliant

--- DSA/1/5_str.py
+++ DSA/1/5_str.py
@@ -1,16 +1,27 @@
-str1 = input('Str1 : ')
-str2 = input('Str2 : ')
-str3 = input('Str3 : ')
-i = 0
-while i < len(str1) :
-	if str1[i]==str2[0]:
-		found = True
-		for j in range(1, len(str2)):
-			if not str1[i+j]==str2[j]:
-				found = False
-				break
-		if found:
-			str1 = str1[:i]+str3+str1[i+len(str2):]
-			i = i + len(str3) - 1
-	i = i + 1
-print('str  : ' + str1)
+def main():
+    str1 = input('Str1 : ')
+    str2 = input('Str2 : ')
+    str3 = input('Str3 : ')
+    print('string  : ' + replace(str1, str2, str3))
+
+
+def replace(string, query, replacer):
+    if len(query) > len(string):
+        return string
+    i = 0
+    while i < len(string):
+        if string[i] == query[0]:
+            found = True
+            for j in range(1, len(query)):
+                if not string[i + j] == query[j]:
+                    found = False
+                    break
+            if found:
+                string = string[:i] + replacer + string[i + len(query):]
+                i = i + len(replacer) - 1
+        i = i + 1
+    return string
+
+
+if __name__ == '__main__':
+    main()
