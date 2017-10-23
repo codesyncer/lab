8efd82025176babb55911b2f956c182515b3eeb1
srinskit <srinagrao2007@gmail.com>
2017-10-24 00:04:28 +0530

    Search to Present

--- DSA/8/1a.py
+++ DSA/8/1a.py
@@ -7,7 +7,7 @@ for word in my_list:
     my_trie.insert(word)
 test_list = throws + my_list
 for word in test_list:
-    if my_trie.search(word):
+    if my_trie.present(word):
         msg = ' is present'
     else:
         msg = ' is not present'
