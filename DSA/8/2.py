4ab5e47afef9f707873065391b91f37789848faa
srinskit <srinagrao2007@gmail.com>
2017-10-24 00:03:57 +0530

    Tester for 2

---
+++ DSA/8/2.py
@@ -0,0 +1,24 @@
+from Trie import *
+
+
+def main():
+    my_trie = Trie()
+    file = open('text.txt', 'r')
+    word = ''
+    line_c = 1
+    for line in file:
+        col_c = 1
+        for ch in line:
+            if ch.isalpha():
+                word += ch
+            elif len(word) > 0:
+                my_trie.insert(word, (line_c, col_c))
+                word = ''
+            col_c += 1
+        line_c += 1
+    file.close()
+    print(my_trie.search('the'))
+
+
+if __name__ == '__main__':
+    main()
