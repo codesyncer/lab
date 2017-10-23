d79b3f4c8601a35a19c00116881f6c002a81780c
srinskit <srinagrao2007@gmail.com>
2017-10-23 22:05:25 +0530

    Replaced $ with \0; better delimiter

--- DSA/8/Trie.py
+++ DSA/8/Trie.py
@@ -3,7 +3,7 @@ from NiceTrie import *
 
 class Trie(NiceTrie):
     def insert(self, string):
-        super(Trie, self).insert(string + '$')
+        super(Trie, self).insert(string + '\0')
 
     def search(self, string):
-        return super(Trie, self).search(string + '$')
+        return super(Trie, self).search(string + '\0')
