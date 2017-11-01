730f04309e711aa48e9629b69d3cb3cfbe62fdfb
srinskit <srinagrao2007@gmail.com>
2017-11-01 23:07:11 +0530

    Better looks

--- DSA/9_10/Graph.py
+++ DSA/9_10/Graph.py
@@ -7,108 +7,81 @@ class Graph:
             self.label = label
             self.distance = distance
             self.visited = False
+            self.start_time = None
+            self.stop_time = None
             self.predecessor = None
-
-        def brief(self):
-            return self.label, self.distance
 
         def visit(self, from_node=None):
             self.distance = 0 if from_node is None else from_node.distance + 1
             self.visited = True
             self.predecessor = from_node
 
-    class Node:
-        def __init__(self, label, distance=-1):
-            self.label = label
-            self.time = None
-            self.visited = False
-            self.predecessor = None
-
-        def visit(self, from_node=None):
-            self.visited = True
-            self.predecessor = from_node
-
-        def start(self, time):
-            self.time = (time,)
-
-        def stop(self, time):
-            self.time = (self.time[0], time)
-
-    def __init__(self, vertex_c, edges, directed=False, simple=True):
-        self.vertex_c = vertex_c
-        self.edge_list = edges
+    def __init__(self, n_vertices, edge_list, directed=False, simple=True):
+        self.n_vertices = n_vertices
+        self.edge_list = edge_list
         self.directed = directed
         self.simple = simple
         self.adjacency_list = None
         self.adjacency_mat = None
         self.dfs_time = None
+        self.vertices = [Graph.Vertex(i) for i in range(n_vertices)]
 
     @staticmethod
     def input_graph():
         n = int(input('Enter the number of vertices: '))
-        print('Enter the edges(u v):')
-        edges = []
+        print('Enter the edge_list(u v):')
+        edge_list = []
         while True:
             ip = input()
             try:
                 u, v = tuple(ip.split())
-                edges.append((int(u), int(v)))
+                edge_list.append((int(u), int(v)))
             except ValueError:
                 break
-        return n, edges
+        return n, edge_list
 
-    def adjacent_vertices(self, label):
-        if self.adjacency_list is None:
-            self.adjacency_list = Graph.gen_adjacency_list(self.vertex_c, self.edge_list, self.directed, self.simple)
-        return self.adjacency_list[label]
+    def make_adjacency_list(self):
+        adjacency_list = [[] for _ in range(self.n_vertices)]
+        for u, v in self.edge_list:
+            if u < self.n_vertices or v < self.n_vertices:
+                if self.simple or v not in self.vertices[u]:
+                    adjacency_list[u].append(v)
+                    if not self.directed:
+                        adjacency_list[v].append(u)
+        self.adjacency_list = adjacency_list
+        return self.vertices
 
-    def get_adjacency_mat(self):
-        if self.adjacency_mat is None:
-            self.adjacency_mat = Graph.gen_adjacency_mat(self.vertex_c, self.edge_list, self.directed)
-        return self.adjacency_mat
-
-    def get_adjacency_list(self):
-        if self.adjacency_list is None:
-            self.adjacency_list = Graph.gen_adjacency_list(self.vertex_c, self.edge_list, self.directed, self.simple)
-        return self.adjacency_list
-
-    @staticmethod
-    def gen_adjacency_list(vertex_c, edges, directed=False, simple=True):
-        node_list = [[] for _ in range(vertex_c)]
-        for u, v in edges:
-            if u < vertex_c or v < vertex_c:
-                if simple or v not in node_list[u]:
-                    node_list[u].append(v)
-                    if not directed:
-                        node_list[v].append(u)
-        return node_list
-
-    @staticmethod
-    def gen_adjacency_mat(vertex_c, edges, directed=False):
-        mat = [[0 for _ in range(vertex_c)] for _ in range(vertex_c)]
-        for u, v in edges:
-            if u < vertex_c or v < vertex_c:
+    def make_adjacency_mat(self):
+        mat = [[0 for _ in range(self.n_vertices)] for _ in range(self.n_vertices)]
+        for u, v in self.edge_list:
+            if u < self.n_vertices or v < self.n_vertices:
                 mat[u][v] += 1
-                if not directed:
+                if not self.directed:
                     mat[v][u] += 1
+        self.adjacency_mat = mat
         return mat
 
-    def connected_bfs(self, src_label):
-        graph = self.get_adjacency_list()
-        node_list = [Graph.Vertex(i) for i in range(len(graph))]
-        return self.pre_initialized_bfs(node_list, src_label)
+    def initialize_vertices(self):
+        for vertex in self.vertices:
+            vertex.distance = 0
+            vertex.predecessor = vertex.start_time = vertex.stop_time = None
+            vertex.visited = False
 
-    def pre_initialized_bfs(self, node_list, src_label):
-        src_label_vertex = node_list[src_label]
-        src_label_vertex.visit()
+    def bfs_connected(self, source_label):
+        self.initialize_vertices()
+        return self.bfs(source_label)
+
+    def bfs(self, source_label):
+        source_vertex = self.vertices[source_label]
+        source_vertex.visit()
         my_queue = queue.Queue()
-        my_queue.put(src_label_vertex)
+        my_queue.put(source_vertex)
         visit_list = []
         while not my_queue.empty():
             vertex = my_queue.get()
-            visit_list.append(vertex.brief())
-            for label in self.adjacent_vertices(vertex.label):
-                v = node_list[label]
+            visit_list.append(vertex)
+            for label in self.adjacency_list[vertex.label]:
+                v = self.vertices[label]
                 if not v.visited:
                     adjacent_vertex = v
                     adjacent_vertex.visit(vertex)
@@ -116,42 +89,42 @@ class Graph:
         return visit_list
 
     def get_components(self):
-        graph = self.get_adjacency_list()
-        node_list = [Graph.Vertex(i) for i in range(len(graph))]
-        visited = [False for _ in range(len(graph))]
+        self.initialize_vertices()
+        component_marker = [0 for _ in range(self.n_vertices)]
         component_list = []
-        for i in range(len(graph)):
-            if not visited[i]:
-                vertices = []
-                visited[i] = True
-                for u, _ in self.pre_initialized_bfs(node_list, i):
-                    visited[u] = True
-                    vertices.append(u)
-                component_list.append(vertices)
-        return component_list
+        component_counter = 1
+        for i in range(self.n_vertices):
+            if not component_marker[i]:
+                connected_vertices = []
+                component_marker[i] = component_counter
+                for u in self.bfs(i):
+                    component_marker[u.label] = component_counter
+                    connected_vertices.append(u)
+                component_list.append(connected_vertices)
+                component_counter += 1
+        return component_list, component_marker
 
     def get_cycles(self):
-        graph = self.get_adjacency_list()
-        node_list = [Graph.Vertex(i) for i in range(len(graph))]
-        source_vertex = node_list[0]
+        self.initialize_vertices()
+        source_vertex = self.vertices[0]
         source_vertex.visit()
         my_queue = queue.Queue()
         my_queue.put(source_vertex)
         cycles = []
         while not my_queue.empty():
             vertex = my_queue.get()
-            for label in graph[vertex.label]:
-                if not node_list[label].visited:
-                    adjacent_vertex = node_list[label]
+            for label in self.adjacency_list[vertex.label]:
+                if not self.vertices[label].visited:
+                    adjacent_vertex = self.vertices[label]
                     adjacent_vertex.visit(vertex)
                     my_queue.put(adjacent_vertex)
                 else:
-                    if node_list[label] == vertex.predecessor:
+                    if self.vertices[label] == vertex.predecessor:
                         continue
-                    v1 = node_list[label]
+                    v1 = self.vertices[label]
                     v2 = vertex
                     cycle = []
-                    if node_list[label].distance != vertex.distance:
+                    if self.vertices[label].distance != vertex.distance:
                         v1 = v1.predecessor
                         cycle.append(label)
                     while v1 != v2:
@@ -165,25 +138,22 @@ class Graph:
                     cycles.append(cycle)
         return cycles
 
-    def connected_dfs(self, src_label):
-        graph = self.get_adjacency_list()
-        node_list = [Graph.Node(i) for i in range(len(graph))]
+    def dfs_connected(self, source_label):
         self.dfs_time = 0
-        return self.dfs(node_list, src_label)
+        self.initialize_vertices()
+        return self.dfs(source_label)
 
-    def get_time(self):
-        self.dfs_time += 1
-        return self.dfs_time
-
-    def dfs(self, node_list, u_label):
-        u = node_list[u_label]
+    def dfs(self, u_label):
+        u = self.vertices[u_label]
         u.visit()
-        u.start(self.get_time())
+        self.dfs_time += 1
+        u.start_time = self.dfs_time
         visit_list = [u]
-        for v_label in self.adjacent_vertices(u_label):
-            v = node_list[v_label]
+        for v_label in self.adjacency_list[u_label]:
+            v = self.vertices[v_label]
             if not v.visited:
-                visit_list += self.dfs(node_list, v.label)
+                visit_list += self.dfs(v.label)
                 v.predecessor = u
-        u.stop(self.get_time())
+        self.dfs_time += 1
+        u.stop_time = self.dfs_time
         return visit_list
