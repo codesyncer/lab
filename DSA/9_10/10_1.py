3d29a0fbc26aba3d169705ba6c0ddca723f419fd
srinskit <srinagrao2007@gmail.com>
2017-11-01 23:07:28 +0530

    DFS

--- DSA/9_10/10_1.py
+++ DSA/9_10/10_1.py
@@ -3,9 +3,10 @@ from Graph import *
 
 def main():
     graph = Graph(7, [(1, 2), (0, 1), (2, 3), (2, 5), (3, 5), (3, 6), (3, 4), (4, 6)])
-    for vertex in graph.connected_dfs(2):
+    graph.make_adjacency_list()
+    for vertex in graph.dfs_connected(2):
         print(vertex.label, end=' ')
-        print('Time : ' + str(vertex.time))
+        print('Time : ' + str(vertex.start_time) + ',' + str(vertex.stop_time))
 
 
 if __name__ == '__main__':
